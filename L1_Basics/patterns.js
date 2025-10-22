b07697690dc0e123611a39110caf9df4c63b1670
Shreesh Lahauriya <shreeshlahauriya15@gmail.com>
2025-10-22 23:41:10 +0530

    symmetrical pyramids

--- L1_Basics/patterns.js
+++ L1_Basics/patterns.js
@@ -53,16 +53,26 @@ pattern5(5);
 
 function pattern6(n){
   for(let i =0 ; i<n; i++){
-    for(let j=0; j<n-i+1; j++){
+    for(let j=0; j<n-i-1; j++){
       process.stdout.write(' ');
     }
     for(let j=0; j<2*i+1; j++){
       process.stdout.write('*');
     }
-    for(let j=0; j<n-i+1; j++){
+    for(let j=0; j<n-i-1; j++){
       process.stdout.write(' ');
     }
     process.stdout.write('\n');
   }
 }
 pattern6(5)
+
+function pattern7(n){
+  for (let i = n-1; i>=0; i--) {
+    process.stdout.write(' '.repeat(n-1-i));
+    process.stdout.write('*'.repeat(2*i+1));
+    process.stdout.write(' '.repeat(n-1-i));
+    process.stdout.write('\n');
+  }
+}
+pattern7(5);
