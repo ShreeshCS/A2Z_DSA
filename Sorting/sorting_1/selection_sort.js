700981477031f1bd62e776bc178b97f356c58192
Shreesh Lahauirya <shreeshlahauriya15@gmail.com>
2026-01-24 22:19:41 +0530

    changes to selectiong sort optimization

--- Sorting/sorting_1/selection_sort.js
+++ Sorting/sorting_1/selection_sort.js
@@ -15,9 +15,10 @@ function selectionSort(arr = []) {
 		let minIndex = i;
 		for (let j = i + 1; j < arr.length; j++) {
 			if (arr[j] < arr[minIndex]) {
-				[arr[j], arr[minIndex]] = [arr[minIndex], arr[j]];
+				minIndex = j;
 			}
 		}
+		[arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
 	}
 
 	return arr;
