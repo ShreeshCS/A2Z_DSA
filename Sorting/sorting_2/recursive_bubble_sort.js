25fc585c52c5bb1d4f52088da6e44007b9c6b9c5
Shreesh Lahauirya <shreeshlahauriya15@gmail.com>
2026-01-25 16:49:08 +0530

    Optimize recursive bubble sort by adding a swap check to reduce unnecessary recursive calls

--- Sorting/sorting_2/recursive_bubble_sort.js
+++ Sorting/sorting_2/recursive_bubble_sort.js
@@ -3,11 +3,14 @@ export function recBubbleSort(arr, n) {
 	if (n === 1) return arr;
 
 	let i = 0;
+	let isSwapped = false;
 	while (i < n - 1) {
 		if (arr[i] > arr[i + 1]) {
 			[arr[i + 1], arr[i]] = [arr[i], arr[i + 1]];
+			isSwapped = true;
 		}
 		i++;
 	}
+	if (!isSwapped) return arr;
 	return recBubbleSort(arr, n - 1); // Ensure the sorted array is returned
 }
